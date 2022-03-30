2c828e1d31905b8382a2c5c54bb4dc2c6d572276
Nurlailiyah Salsabilah Valentina <salsabilavalen@student.telkomuniversity.ac.id>
2022-03-30 18:36:23 +0700

    Menambahkan view binding

--- app/src/main/java/org/d3if2092/assessment1/MainActivity.kt
+++ app/src/main/java/org/d3if2092/assessment1/MainActivity.kt
@@ -2,10 +2,15 @@ package org.d3if2092.assessment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if2092.assessment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
