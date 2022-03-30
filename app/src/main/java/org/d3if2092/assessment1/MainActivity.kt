5b47b9e90b92379a8229c08c84525d0ec4b0651b
Nurlailiyah Salsabilah Valentina <salsabilavalen@student.telkomuniversity.ac.id>
2022-03-30 23:38:24 +0700

    Membaca input user

--- app/src/main/java/org/d3if2092/assessment1/MainActivity.kt
+++ app/src/main/java/org/d3if2092/assessment1/MainActivity.kt
@@ -2,6 +2,8 @@ package org.d3if2092.assessment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import org.d3if2092.assessment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener { dataMahasiswa() }
+    }
+
+    private fun dataMahasiswa() {
+        val nama = binding.namaEditText.text.toString()
+        if (TextUtils.isEmpty(nama)) {
+            Toast.makeText(this, R.string.nama_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val nim = binding.nimEditText.text.toString()
+        if (TextUtils.isEmpty(nim)) {
+            Toast.makeText(this, R.string.nim_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val kelas = binding.kelas.text.toString()
+        if (TextUtils.isEmpty(kelas)) {
+            Toast.makeText(this, R.string.kelas_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        binding.textView4.text = getString(R.string.nama_mhs, nama)
+        binding.textView6.text = getString(R.string.nim_mhs, nim)
+        binding.textView8.text = getString(R.string.kelas_mhs, kelas)
     }
 }
